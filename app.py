114b6a575ea196efdc83bbc2d320912523c238e6
priyanshujain31 <78251925+priyanshujain31@users.noreply.github.com>
2021-05-11 13:41:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 st.set_option('deprecation.showfileUploaderEncoding', False)
 # Load the pickled model
-pickle_in = open("/content/drive/My Drive/decision_priyanshu_midterm.pkl","rb")
+pickle_in = open("decision_priyanshu_midterm.pkl","rb")
 model=pickle.load(pickle_in)
-dataset= pd.read_csv('/content/drive/My Drive/Priyanshu Jain - Classification Dataset1.csv')
+dataset= pd.read_csv('Priyanshu Jain - Classification Dataset1.csv')
 X = dataset.iloc[:,1:10].values
 
 # Taking care of missing data
