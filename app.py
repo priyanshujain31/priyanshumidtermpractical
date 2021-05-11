03a0f2367f0730cd8931ca378df0c87de3127294
priyanshujain31 <78251925+priyanshujain31@users.noreply.github.com>
2021-05-11 13:48:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -74,8 +74,8 @@ def main():
     HasCrCard = st.number_input('Insert a HasCrCard 0 For No 1 For Yes',0,1)
     Tenure = st.number_input('Insert a Tenure',0,20)
     Balance = st.number_input('Insert a Balance',0)
-    Gender = st.number_input('Insert 0 For Male 1 For Female ')
-    Geography= st.number_input('Insert Geography 0 For France 1 For Spain')
+    Gender = st.number_input('Insert 0 For Male 1 For Female ',0,1)
+    Geography= st.number_input('Insert Geography 0 For France 1 For Spain',0,1)
     IsActiveMember= st.number_input('Insert a IsActiveMember 0 For No 1 For Yes',0,1)
     EstimatedSalary= st.number_input('Insert a EstimatedSalary',0)
     
